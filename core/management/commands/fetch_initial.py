0898ce51ee5322c6b6f4768fbb262eb6ad5d7558
Daniel Cichowski <daniel.cichowski@materna.com.pl>
2018-06-22 16:10:10 +0200

    add management command for fetching initial data

---
+++ core/management/commands/fetch_initial.py
@@ -0,0 +1,11 @@
+import requests
+
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+
+	help = 'fetch initial data from external api'
+	def handle():
+		pass
+
